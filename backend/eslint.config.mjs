e76c207c477a6016de3fc429ae21d82587c0eec6
Euwamahoro <e.uwamahoro@alustudent.com>
2025-07-22 23:37:39 +0200

    correct lint configuration

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -4,8 +4,7 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.node } },
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
