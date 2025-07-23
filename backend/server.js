e63934423f99ed17ca530968e67cf48d88061a94
Euwamahoro <e.uwamahoro@alustudent.com>
2025-07-23 14:48:13 +0200

    corrected db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./db');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
